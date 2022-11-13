56d6ec1e3284852aab170b5472626f76a3f5ee1e
R3X <73672753+tboy-hacker@users.noreply.github.com>
2022-11-13 00:17:11 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ chmod +x pickle.sh
 chmod +x scan.sh
 sudo gem install XSpear
 pip3 install -r requirements.txt
-sudo apt install nyancat
-sudo apt install joomscan
-sudo apt install wpscan
+sudo apt install nyancat -y
+sudo apt install joomscan -y
+sudo apt install wpscan -y
 echo "" "$Green"
 echo "Done let's have a pickle"
